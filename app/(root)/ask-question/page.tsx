2e07a9db50e2687bc432964703405e73f567577d
Suryana <nanasuryana335@gmail.com>
2023-12-07 16:42:16 +0700

    feat(ask-question/page.tsx): use useUser to get clerkID

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -5,34 +5,37 @@ import User from "@/database/user.model";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import { useUser } from "@clerk/nextjs";
 
 const Page = () => {
   const router = useRouter();
-  const userID = "1234567890";
-  const [user, setUser] = useState<User | null>(null);
+
+  const { user } = useUser();
+
+  const [appUser, setAppUser] = useState<User | null>(null);
 
   useEffect(() => {
-    if (!userID) router.push("/sign-in");
+    if (!user?.id) router.push("/sign-in");
 
     const fetchUser = async () => {
       const response = await axios.get(
-        `https://dev-overflow-backend-1a8b01b9d384.herokuapp.com/api/v1/user/${userID}`
+        `https://dev-overflow-backend-1a8b01b9d384.herokuapp.com/api/v1/user/${user?.id}`
       );
       const mongoUser = response.data;
       console.log(mongoUser);
-      setUser(mongoUser);
+      setAppUser(mongoUser);
     };
 
     fetchUser();
   }, []);
 
-  if (!user) return <div>Loading...</div>;
+  if (!appUser) return <div>Loading...</div>;
 
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
       <div className="mt-9">
-        <Question mongoUserId={JSON.stringify(user.clerkID)} />
+        <Question mongoUserId={JSON.stringify(user?.id)} />
       </div>
     </div>
   );
